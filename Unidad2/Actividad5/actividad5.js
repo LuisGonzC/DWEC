05822c0da7f48a4afe23fc5c8bbf30e8f5dc1500
LuisGonzC <luigoncue@alu.edu.gva.es>
2023-10-02 16:52:51 +0200

    Update - Actividad5 U2

---
+++ Unidad2/Actividad5/actividad5.js
@@ -0,0 +1,9 @@
+let nombre = prompt("Indica tu nombre");
+let apellidos = prompt("Indica tus apellidos");
+let salario = parseFloat(prompt("Indica tu salario"));
+let edad = parseInt(prompt("Indica tu edad"));
+
+
+
+alert('Nombre: ' + nombre + '\nApellidos: ' + apellidos +
+'\nEdad: ' + edad + ' Salario: ' + salario);
